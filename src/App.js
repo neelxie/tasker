18fc67c203b958a1327bada58b72498f5a668d91
neelxie <kidricederek@gmail.com>
2021-09-20 08:38:24 +0300

    Add new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <Header />
+      <div className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      </div>
     </div>
   );
 }
