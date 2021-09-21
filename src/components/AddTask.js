b351e1acb6d431908da555c2ed83e6ef635bbf07
neelxie <kidricederek@gmail.com>
2021-09-21 05:30:34 +0300

    Add new changes

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,24 +1,34 @@
-import React, { useState} from 'react'
+import React, { useState } from "react";
 
 export const AddTask = () => {
-    const [text, setText] = useState('')
-    const [day, setDay] = useState('')
-    const [reminder, setReminder] = useState(false)
-    return (
-        <form className='add-form'>
-            <div className='form-control'>
-                <label>Task</label>
-                <input  type='text' value={text} onChange={(e) => setText(e.target.value)}placeholder='Add Task'/>
-            </div>
-            <div className='form-control'>
-                <label>Day & Time</label>
-                <input  type='text' placeholder='Add Day & Time'/>
-            </div>
-            <div className='form-control form-control-check'>
-                <label>Reminder</label>
-                <input  type='checkbox' />
-            </div>
-            <input type='submit' className='btn btn-block'value='Add Task'/>
-        </form>
-    )
-}
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Add Task"
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+          placeholder="Add Day & Time"
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Reminder</label>
+        <input type="checkbox" value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+      </div>
+      <input type="submit" className="btn btn-block" value="Add Task" />
+    </form>
+  );
+};
