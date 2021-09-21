1a7c745181c18dbceee71751fcd2e205a6d36d56
neelxie <kidricederek@gmail.com>
2021-09-21 05:21:09 +0300

    Add new changes

---
+++ src/components/AddTask.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddTask = () => {
+    return (
+        <form className='add-form'>
+            <div className='form-control'>
+                <label></label>
+            </div>
+        </form>
+    )
+}
